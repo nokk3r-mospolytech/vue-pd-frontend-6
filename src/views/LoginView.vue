bb30c5142cc434aa993fa71f64f0a334b8c090bf
nokk3r <nokk3r@mail.ru>
2022-06-09 04:38:35 +1100

    fixed login form buttons

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -9,9 +9,13 @@
             <input type="password">
             <span>Забыли пароль?</span>
          </div>
-         <div class="ul buttons">
-            <input type="submit" value="войти">
-            <input type="submit" value="регистрация">
+         <div class="ul ul_buttons">
+            <router-link class="buttons buttons_submit" to="/">
+               <span>Войти</span>
+            </router-link>
+            <router-link class="buttons" to="/register">
+               <span>Регистрация</span>
+            </router-link>
          </div>
       </form>
    </div>
@@ -69,14 +73,67 @@ export default {
          }
       }
 
-      .buttons{
+      .ul_buttons {
          justify-content: space-between;
          align-items: center;
-         width: 70%;
+         width: 65%;
          margin-top: 30px;
 
-         input{
+         input {
             width: 47%;
+         }
+
+         .buttons {
+            width: 47%;
+            padding: 5px;
+            border: 1px solid black;
+            border-radius: 5px;
+            justify-content: center;
+         }
+
+         .buttons_submit {
+            background: black;
+            color: white;
+         }
+      }
+   }
+}
+
+@media (max-width: 767px) {
+   .login {
+      form {
+         width: 100%;
+         margin: 0;
+
+         > span {
+            width: 100%;
+         }
+
+         input {
+            width: 100%;
+         }
+
+         .password {
+            flex-direction: column;
+
+            input {
+               width: 100%;
+            }
+
+            span {
+               margin-top: 25px;
+            }
+         }
+
+         .ul_buttons {
+            width: 100%;
+            margin-top: 10px;
+            flex-direction: column;
+
+            .buttons {
+               width: 65%;
+               margin-top: 35px;
+            }
          }
       }
    }
